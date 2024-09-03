024dc4e7f11d493f818403ae60192b1d2ea14641
kelvinau125 <topremaxx@gmail.com>
2024-09-03 23:36:58 +0800

    comment bug

--- src/components/Post/Comment.vue
+++ src/components/Post/Comment.vue
@@ -3,8 +3,8 @@
     <div class="flex mb-4 bg-secondTheme px-4 py-2 rounded-lg">
       <img :src="userAvatar" alt="User Avatar" class="w-12 h-12 rounded-full mr-4" />
       <input v-model="newComment" type="text" placeholder="Reply the post..."
-        class="flex-grow p-2 border-none inputfield-color" @keyup.enter="addComment()" />
-      <button @click="addComment" class="text-white text-2xl rounded-r-lg">
+        class="flex-grow p-2 border-none inputfield-color" @keyup.enter="addComment()" :disabled="isPosting" />
+      <button @click="addComment" class="text-white text-2xl rounded-r-lg" :disabled="isPosting">
         <i class="fas fa-paper-plane"></i>
       </button>
     </div>
@@ -48,11 +48,20 @@ export default {
       ],
 
       postID: this.$route.query.postID,
+
+      isPosting: false, 
     };
   },
   mounted() {
     this.getUserInfoApi()
     this.getCommentApi()
+
+    // Call getCommentApi every 5 seconds
+    this.commentInterval = setInterval(this.getCommentApi, 5000);
+  },
+  beforeDestroy() {
+    // Clear the interval when the component is destroyed
+    clearInterval(this.commentInterval);
   },
   methods: {
     async getUserInfoApi() {
@@ -62,32 +71,77 @@ export default {
       this.userAvatar = result.userAvatar;
     },
     async addComment() {
-      const result = await commentPost(this.postID, this.newComment);
+      if (this.newComment.trim() === "") {
+        return;
+      }
 
-      if (result) {
-        this.comments.unshift({
-          id: this.comments.length + 1,
-          username: this.username,
-          avatar: this.userAvatar,
-          text: this.newComment,
-          duration: "1 seconds ago",
-        });
-        this.newComment = "";
+      this.isPosting = true; 
 
+      await commentPost(this.postID, this.newComment);
+
+      this.comments.unshift({
+        id: this.comments.length + 1,
+        username: this.username,
+        avatar: this.userAvatar,
+        text: this.newComment,
+        duration: "1 seconds ago",
+      });
+
+      this.newComment = "";
+
+      this.isPosting = false;
+    },
+
+    // without time interval
+    // async getCommentApi() {
+    //   this.getList = await getCommentList(this.postID);
+
+    //   for (let i = 0; i < this.getList.length; i++) {
+    //     this.comments.push({
+    //       id: i + 1,
+    //       username: this.getList[i]["user"]["firstName"] + " " + this.getList[i]["user"]["lastName"],
+    //       avatar: this.getList[i]["user"]["userAvatar"],
+    //       text: this.getList[i]["comment"],
+    //       duration: this.getList[i]["duration"],
+    //     });
+    //   }
+    // },
+
+    async getCommentApi() {
+      const newList = await getCommentList(this.postID);
+
+      // Check if there's a change in the comment list
+      if (this.isListChanged(newList)) {
+        this.comments = newList.map((comment, index) => ({
+          id: index + 1,
+          username: comment.user.firstName + " " + comment.user.lastName,
+          avatar: comment.user.userAvatar,
+          text: comment.comment,
+          duration: comment.duration,
+        }));
       }
     },
-    async getCommentApi() {
-      this.getList = await getCommentList(this.postID);
-
-      for (let i = 0; i < this.getList.length; i++) {
-        this.comments.push({
-          id: i + 1,
-          username: this.getList[i]["user"]["firstName"] + " " + this.getList[i]["user"]["lastName"],
-          avatar: this.getList[i]["user"]["userAvatar"],
-          text: this.getList[i]["comment"],
-          duration: this.getList[i]["duration"],
-        });
+    isListChanged(newList) {
+      // Check if the length of both lists is different
+      if (newList.length !== this.comments.length) {
+        return true;
       }
+
+      // Check if each comment in the new list is different from the current list
+      for (let i = 0; i < newList.length; i++) {
+        if (
+          newList[i].user.firstName !== this.comments[i].username ||
+          newList[i].user.lastName !== this.comments[i].username ||
+          newList[i].user.userAvatar !== this.comments[i].avatar ||
+          newList[i].comment !== this.comments[i].text ||
+          newList[i].duration !== this.comments[i].duration
+        ) {
+          return true;
+        }
+      }
+
+      // If no differences found
+      return false;
     },
   }
 };
